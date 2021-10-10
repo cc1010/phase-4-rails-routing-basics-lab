97727b32433007e853d4a43469679b5e7ee60f93
Chaim Chazan <chaimyisroel@gmail.com>
2021-10-10 16:01:43 -0400

    commit tree 4b07256695511b53372549faeb1b2d32c21de934

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+   def index
+      students = Student.all 
+      render json: students
+   end
+
+   def grades
+      grades = Student.order("grade DESC")
+      render json: grades
+   end
+
+   def highest_grade
+      grades = Student.order("grade DESC").first
+      render json: grades
+   end
+end
